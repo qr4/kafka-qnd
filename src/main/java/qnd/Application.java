a72739af4758013465d301a125d7780194703a67
Vitali Henne <vitali.henne@freiheit.com>
2018-03-21 17:44:45 +0100

    fix minor errors

--- src/main/java/qnd/Application.java
+++ src/main/java/qnd/Application.java
@@ -1,6 +1,7 @@
 package qnd;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.springframework.boot.SpringApplication;
@@ -13,11 +14,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static final int NUM_POLLING_CONSUMERS = 5;
 
-    public static void main( final String[] args ) throws Exception {
+    public static void main( final String[] args ) {
         // create and start consumers
         IntStream.range( 0, NUM_POLLING_CONSUMERS ).boxed()
                 .map( i -> new KafkaPollingConsumer() )
-                .map( CompletableFuture::runAsync );
+                .map( CompletableFuture::runAsync )
+                .collect( Collectors.toList());
+        new KafkaStreamsConsumer().start();
 
         SpringApplication.run( Application.class, args );
     }
---
+++ src/main/java/qnd/KafkaStreamsConsumer.java
@@ -0,0 +1,38 @@
+package qnd;
+
+import java.util.Properties;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Vitali Henne (vitali.henne@freiheit.com).
+ */
+public class KafkaStreamsConsumer {
+    private static final Logger LOG = LoggerFactory.getLogger( KafkaStreamsConsumer.class );
+    private final String TOPIC = "qnd";
+    private KafkaStreams kafkaStreams;
+
+    public KafkaStreamsConsumer() {
+        final Properties props = new Properties();
+        props.setProperty( StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "http://localhost:9092" );
+        props.setProperty( StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName() );
+        props.setProperty( StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName() );
+        props.setProperty( StreamsConfig.APPLICATION_ID_CONFIG, "KafkaStreamsConsumer" );
+
+
+        // TODO implement me:
+        // 1. create a StreamsBuilder
+        // 2. stream from our TOPIC
+        // 3. use forEach to print all messages
+        // 4. create the KafkaStreams from the Builder and the props
+    }
+
+    void start() {
+        kafkaStreams.start();
+        Runtime.getRuntime().addShutdownHook( new Thread( kafkaStreams::close ) );
+    }
+}
